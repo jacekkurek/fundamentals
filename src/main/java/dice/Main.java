ca242d0114a61e8dbf33fba44011bca0b6583f99
jacekkurek <tenjacekkurek@gmail.com>
2018-12-10 17:05:03 +0100

    Mofify dice
    Class redesigned.

--- src/main/java/dice/Main.java
+++ src/main/java/dice/Main.java
@@ -1,54 +1,61 @@
 package dice;
 
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
+    private static void welcomeMsg(){
+        System.out.println("Let's play a game.\nDescribe what to throw");
+    }
 
     public static void main(String[] args) {
 
 
+
+        welcomeMsg();
         Scanner scanner = new Scanner(System.in);
-        String input;
-        input = scanner.next();
-        int dPosition = 0;
-        while (input.toLowerCase().indexOf('d') < 0) {
-            System.out.println(dPosition);
-            System.out.println("\"D\" is missing");
+        String input = scanner.next().toLowerCase();
+        Pattern validPattern = Pattern.compile("\\d*d\\d+((-|\\+)\\d+)?");
+        Matcher matcher = validPattern.matcher(input);
+        Random random = new Random();
+        int a;
+        int b;
+        int c;
+
+
+        while (!matcher.matches()) {
+            System.out.println("Invalid format");
             scanner.next();
         }
-        dPosition = input.toLowerCase().indexOf('d');
 
-        int plusPosition = input.indexOf("+");
-        int minusPosition = input.indexOf("-");
-        int modifierPosition;
-        int result = 0;
-        int throwNumber;
-        Random random = new Random();
-
-        if (plusPosition > -1) {
-            modifierPosition = plusPosition;
-            result += Integer.parseInt(input.substring(modifierPosition + 1, input.length()));
-        } else if (minusPosition > -1) {
-            modifierPosition = minusPosition;
-            result -= Integer.parseInt(input.substring(modifierPosition + 1, input.length()));
+        String[] s1 = input.split("d");
+        if (s1[0].equals("")) {
+            a = 1;
         } else {
-            modifierPosition = input.length();
+            a = Integer.parseInt(s1[0]);
         }
-        if (dPosition > 0) {
-            throwNumber = Integer.parseInt(input.substring(0, dPosition));
+
+        if (input.contains("+")) {
+            String[] s2 = s1[1].split("\\+");
+            b = Integer.parseInt(s2[0]);
+            c = Integer.parseInt(s2[1]);
+        } else if (input.contains("-")) {
+            String[] s2 = s1[1].split("-");
+            b = Integer.parseInt(s2[0]);
+            c = -Integer.parseInt(s2[1]);
         } else {
-            throwNumber = 1;
-        }
-        int diceType = Integer.parseInt(input.substring(dPosition + 1, modifierPosition));
-
-        if (throwNumber > 0 && diceType > 0) {
-            for (int i = 0; i < throwNumber; i++) {
-                result += random.nextInt(diceType);
-            }
+            b = Integer.parseInt(s1[1]);
+            c = 0;
         }
 
-        System.out.println("result " + result);
+        System.out.println(a + random.nextInt(b + 1) + c);
+
+
+
     }
+
+
 }
+
