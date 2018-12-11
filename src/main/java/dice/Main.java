c165079efb90c0cf3f574d96e923d06bd1b001b4
jacekkurek <tenjacekkurek@gmail.com>
2018-12-11 11:12:03 +0100

    extract methods in dice and add repeat prompt

--- src/main/java/dice/Main.java
+++ src/main/java/dice/Main.java
@@ -6,56 +6,73 @@ import java.util.regex.Pattern;
 
 public class Main {
 
-    private static void welcomeMsg(){
+    private static void welcomeMsg() {
         System.out.println("Let's play a game.\nDescribe what to throw");
     }
 
-    public static void main(String[] args) {
-
-
-
-        welcomeMsg();
+    private static String readAndValidateInput() {
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.next().toLowerCase();
-        Pattern validPattern = Pattern.compile("\\d*d\\d+((-|\\+)\\d+)?");
-        Matcher matcher = validPattern.matcher(input);
-        Random random = new Random();
-        int a;
-        int b;
-        int c;
+        Pattern validPattern = Pattern.compile("\\d*d\\d+(([-+])\\d+)?");
 
+        String input = scanner.next().toLowerCase();
+        Matcher matcher = validPattern.matcher(input);
 
         while (!matcher.matches()) {
             System.out.println("Invalid format");
-            scanner.next();
+            input = scanner.next().toLowerCase();
+            matcher = validPattern.matcher(input);
         }
+        return input;
+    }
 
+    private static int[] parseInput(String input) {
+        int[] result = new int[3];
         String[] s1 = input.split("d");
         if (s1[0].equals("")) {
-            a = 1;
+            result[0] = 1;
         } else {
-            a = Integer.parseInt(s1[0]);
+            result[0] = Integer.parseInt(s1[0]);
         }
 
         if (input.contains("+")) {
             String[] s2 = s1[1].split("\\+");
-            b = Integer.parseInt(s2[0]);
-            c = Integer.parseInt(s2[1]);
+            result[1] = Integer.parseInt(s2[0]);
+            result[2] = Integer.parseInt(s2[1]);
         } else if (input.contains("-")) {
             String[] s2 = s1[1].split("-");
-            b = Integer.parseInt(s2[0]);
-            c = -Integer.parseInt(s2[1]);
+            result[1] = Integer.parseInt(s2[0]);
+            result[2] = -Integer.parseInt(s2[1]);
         } else {
-            b = Integer.parseInt(s1[1]);
-            c = 0;
+            result[1] = Integer.parseInt(s1[1]);
         }
-
-        System.out.println(a + random.nextInt(b + 1) + c);
-
-
-
+        return result;
     }
 
+    private static void resultMsg(int[] result, Random random) {
+        System.out.println(result[0] + random.nextInt(result[1]) + result[2]);
+    }
 
+    private static char repeatPrompt() {
+        char repeat;
+        do {
+            System.out.println("Play again? y / n");
+            Scanner scanner = new Scanner(System.in);
+            repeat = scanner.next().toLowerCase().charAt(0);
+        } while (repeat != 'y' && repeat != 'n');
+        return repeat;
+    }
+
+    public static void main(String[] args) {
+
+        char repeat;
+        Random random = new Random();
+        do {
+            welcomeMsg();
+            String input = readAndValidateInput();
+            int[] result = parseInput(input);
+            resultMsg(result, random);
+            repeat = repeatPrompt();
+        } while (repeat == 'y');
+    }
 }
 
